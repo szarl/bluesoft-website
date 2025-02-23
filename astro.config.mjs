1445881711bafd894bd191baa125b04086b9b7d5
Karol Rutkowski <karolrut123@gmail.com>
2025-02-23 18:35:17 +0100

    fix: add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
-import node from '@astrojs/node';
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/serverless';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   output: 'server',
-  adapter: node({
-    mode: 'standalone',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+    imageService: true,
+    maxDuration: 8,
   }),
   integrations: [
     tailwind(),
