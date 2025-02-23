090edec28ef548def9d9a888d19383012dbebf13
Karol Rutkowski <karolrut123@gmail.com>
2025-02-23 18:23:37 +0100

    fix: add output dir

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,15 @@ import node from '@astrojs/node';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
-    output: 'server',
-    adapter: node({
-      mode: 'standalone'
-    }),
-
-    integrations: [
-        tailwind(),
-    ],
+  output: 'server',
+  adapter: node({
+    mode: 'standalone',
+  }),
+  integrations: [
+    tailwind(),
+  ],
+  build: {
+    outDir: 'dist',
+  },
 });
